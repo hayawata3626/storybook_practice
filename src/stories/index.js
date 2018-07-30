b5028b68e8e535f7c52863fd23c4d74665b71bd3
hayawata3626 <top.men09@gmail.com>
2018-07-31 00:08:56 +0900

    ストーリーにコンポーネントを追加

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,12 +5,17 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import App from '../App';
+
+storiesOf('App', module)
+  .add('デフォルト', () => <App/>)
+  .add('指定したメッセージ', () => <App message="ようこそReactへ" />);
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
+  .add('第二階層', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
